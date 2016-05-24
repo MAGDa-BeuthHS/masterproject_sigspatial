28e0118c7356c7b24a20f8620b94ecc5da60c9ff
Hagen Tarner <hagen.tarner@beuth-hochschule.de>
2016-05-24 18:51:50 +0200

    UT for SimpleTimeSlicer

--- src/main/scala/utils/slicer/time/SimpleTimeSlicer.scala
+++ src/main/scala/utils/slicer/time/SimpleTimeSlicer.scala
@@ -7,17 +7,17 @@ import org.joda.time.{DateTime, Duration}
 class SimpleTimeSlicer extends TimeSlicer {
 
   val formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-  val zero = new DateTime("2015-01-01T00:00:00.000-00:00")
+  val zero = new DateTime("2009-01-01T00:00:00.000-00:00")
 
   override def getSliceForTimestamp(timestampInCsv: String): Int = {
     translateTimestamp(new DateTime(formatter.parse(timestampInCsv)))
   }
 
-  def translateTimestamp(dt: DateTime, zero: DateTime = zero): Int = {
+  private def translateTimestamp(dt: DateTime, zero: DateTime = zero): Int = {
     ((roundDateTime(dt, Duration.standardHours(2)).getMillis - zero.getMillis) / Duration.standardHours(2).getMillis).toInt
   }
 
-  def roundDateTime(t: DateTime, d: Duration): DateTime = {
+  private def roundDateTime(t: DateTime, d: Duration): DateTime = {
     t minus (t.getMillis - (t.getMillis.toDouble / d.getMillis).round * d.getMillis)
   }
 }
