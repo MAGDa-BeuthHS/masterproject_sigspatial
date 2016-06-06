5b1379930a16aa9838b278d1ffcd08f07c5c7c38
Hagen Tarner <hagen.tarner@beuth-hochschule.de>
2016-06-06 17:43:40 +0200

    write stuff to files (csv) and terminal

---
+++ src/main/scala/utils/writer/CsvWriter.scala
@@ -0,0 +1,18 @@
+package utils.writer
+
+import java.io.{PrintWriter, BufferedWriter, File}
+
+import com.typesafe.config.ConfigFactory
+
+class CsvWriter extends Writer {
+  val conf = ConfigFactory.load()
+
+  override def write(data: Array[((Int, Int, Int), Int)], output: String): Unit = {
+    val fqfn = s"$output/${conf.getString("output.filename")}"
+    new PrintWriter(fqfn) {
+      write(s"${conf.getString("output.header")}\n")
+      data.foreach(line => write(s"${line._1._2},${line._1._3},${line._1._1},0,0\n"))
+      close
+    }
+  }
+}
