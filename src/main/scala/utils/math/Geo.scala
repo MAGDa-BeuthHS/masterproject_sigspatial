0ba736c217534ed69bbde9b723a0319c54bb8485
Sebastian Urbanek <sebastian.urbanek@posteo.de>
2016-06-15 18:08:07 +0200

    Forgot to format the code, with this rescheduled.

--- src/main/scala/utils/math/Geo.scala
+++ src/main/scala/utils/math/Geo.scala
@@ -2,17 +2,14 @@ package utils.math
 
 import scala.math.{Pi, cos}
 
-object Geo extends Serializable
-{
+object Geo extends Serializable {
   val aLat = 111300
 
-  def calcDeltaLat(deltaLng: Float, lat: Float): Float =
-  {
+  def calcDeltaLat(deltaLng: Float, lat: Float): Float = {
     calcDistLng(deltaLng, lat) / aLat
   }
 
-  def calcDistLng(deltaLng: Float, lat: Float): Float =
-  {
-    (aLat * cos(lat * (Pi/180)) * deltaLng).toFloat
+  def calcDistLng(deltaLng: Float, lat: Float): Float = {
+    (aLat * cos(lat * (Pi / 180)) * deltaLng).toFloat
   }
 }
