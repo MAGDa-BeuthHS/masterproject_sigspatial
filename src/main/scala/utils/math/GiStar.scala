592a1577d34181d6291dd40a5db89d016d51940c
Sebastian Urbanek <sebastian.urbanek@posteo.de>
2016-06-21 14:09:14 +0200

    Gi* adapted to type double and prepared to calculate p-value.

--- src/main/scala/utils/math/GiStar.scala
+++ src/main/scala/utils/math/GiStar.scala
@@ -3,6 +3,7 @@ package utils.math
 import scala.math.sqrt
 
 object GiStar extends Serializable {
+
   /*
   x: List of all cells
   n: Number of all cells --> Length of x
@@ -11,35 +12,40 @@ object GiStar extends Serializable {
   W: List of all neighbours attributes
    */
 
-  def calcZ(w: List[Int], n: Long, avg: Float, s: Float): Float = {
+  def calcZ(w: List[Int], n: Long, avg: Double, s: Double): Double = {
     val numerator = w.sum - (avg * w.length)
-    val denominator = sqrt((w.length * (n - w.length) * s) / (n - 1)).toFloat
+    val den1 = w.length.toDouble * (n.toDouble - w.length) * s
+    val denominator = sqrt(den1 / (n - 1).toDouble)
     numerator / denominator
   }
 
-  def calcStdDeviation(x: List[Int]): Float = {
+  def calcP(z: Double): Double = {
+    0.0 * z
+  }
+
+  def calcStdDeviation(x: List[Int]): Double = {
     val sumX2 = calcSumXToPowerOf2(x)
     val avg = calcAverageValue(x)
     calcStdDeviation(x.length, avg, sumX2)
   }
 
-  def calcStdDeviation(n: Long, avg: Float, sumX2: Long): Float = {
+  def calcStdDeviation(n: Long, avg: Double, sumX2: Double): Double = {
     // returns s^2 and not s!
-    (sumX2 / n) - (avg * avg)
+    (sumX2 / n.toDouble) - (avg * avg)
   }
 
-  def calcStdDeviation(x: List[Int], avg: Float): Float = {
+  def calcStdDeviation(x: List[Int], avg: Double): Double = {
     val sumX2 = calcSumXToPowerOf2(x)
     calcStdDeviation(x.length, avg, sumX2)
   }
 
-  def calcStdDeviation(x: List[Int], sumX2: Long): Float = {
+  def calcStdDeviation(x: List[Int], sumX2: Long): Double = {
     val avg = calcAverageValue(x)
     calcStdDeviation(x.length, avg, sumX2)
   }
 
-  def calcAverageValue(x: List[Int]): Float = {
-    x.sum / x.length
+  def calcAverageValue(x: List[Int]): Double = {
+    x.sum / x.length.toDouble
   }
 
   def calcSumWToPowerOf2(w: List[Int]): Long = {
@@ -56,38 +62,38 @@ object GiStar extends Serializable {
     sum
   }
 
-  //  def calcZWithFloat(w: List[Float], n: Long, avg: Float, stdDeviation: Float): Float =
-  //  {
-  //    val numerator   = w.sum - (avg * w.length)
-  //    val denominator = sqrt((w.length * (n - w.length) * stdDeviation) / (n - 1)).toFloat
-  //    numerator / denominator
-  //  }
-  //
-  //  def calcStdDeviationWithFloat(x: List[Float]): Float =
-  //  {
-  //    val sumX2 = calcSumXToPowerOf2WithFloat(x)
-  //    val avg = calcAverageValueWithFloat(x)
-  //    calcStdDeviationWithFloat(x.length, avg, sumX2)
-  //  }
-  //
-  //  def calcStdDeviationWithFloat(n: Long, avg: Float, sumX2: Float): Float =
-  //  {
-  //    (sumX2 / n) - (avg * avg)
-  //  }
-  //
-  //  def calcAverageValueWithFloat(x: List[Float]): Float =
-  //  {
-  //    x.sum / x.length
-  //  }
-  //
-  //  def calcSumXToPowerOf2WithFloat(x: List[Float]): Float =
-  //  {
-  //    var i = 0
-  //    var sum = 0f
-  //    while (i < x.length) {
-  //      sum += x(i) * x(i)
-  //      i += 1
-  //    }
-  //    sum
-  //  }
+//    def calcZWithFloat(w: List[Float], n: Long, avg: Float, stdDeviation: Float): Float =
+//    {
+//      val numerator   = w.sum - (avg * w.length)
+//      val denominator = sqrt((w.length * (n - w.length) * stdDeviation) / (n - 1)).toFloat
+//      numerator / denominator
+//    }
+//
+//    def calcStdDeviationWithFloat(x: List[Float]): Float =
+//    {
+//      val sumX2 = calcSumXToPowerOf2WithFloat(x)
+//      val avg = calcAverageValueWithFloat(x)
+//      calcStdDeviationWithFloat(x.length, avg, sumX2)
+//    }
+//
+//    def calcStdDeviationWithFloat(n: Long, avg: Float, sumX2: Float): Float =
+//    {
+//      (sumX2 / n.toFloat) - (avg * avg)
+//    }
+//
+//    def calcAverageValueWithFloat(x: List[Float]): Float =
+//    {
+//      x.sum / x.length.toFloat
+//    }
+//
+//    def calcSumXToPowerOf2WithFloat(x: List[Float]): Float =
+//    {
+//      var i = 0
+//      var sum = 0f
+//      while (i < x.length) {
+//        sum += x(i) * x(i)
+//        i += 1
+//      }
+//      sum
+//    }
 }
--- src/test/scala/utils/GiStarSpec.scala
+++ src/test/scala/utils/GiStarSpec.scala
@@ -2,24 +2,25 @@ package utils
 
 import org.scalatest.{Matchers, WordSpec}
 import utils.math.GiStar
+import scala.math.round
 
 class GiStarSpec extends WordSpec with Matchers
 {
-  val xHelper = List(1, 2, 3, 4, 5, 6, 7)
-  val wHelper = List(4, 5, 6)
+  val x = List(1, 2, 3, 4, 5, 6, 7)
+  val w = List(4, 5, 6)
   "GiStar helper-functions" should
   {
     "return 4 for calcAvaerageValue(x)" in {
-      GiStar.calcAverageValue(xHelper) shouldBe 4
+      GiStar.calcAverageValue(x) shouldBe 4
     }
     "return 140 for calcSumX2(x)" in {
-      GiStar.calcSumXToPowerOf2(xHelper) shouldBe 140
+      GiStar.calcSumXToPowerOf2(x) shouldBe 140
     }
     "return (s^2 =) 4 for calcStdDeviation(x)" in {
-      GiStar.calcStdDeviation(xHelper) shouldBe 4
+      GiStar.calcStdDeviation(x) shouldBe 4
     }
     "return 77 for calcSumW2(w)" in {
-      GiStar.calcSumWToPowerOf2(wHelper) shouldBe 77
+      GiStar.calcSumWToPowerOf2(w) shouldBe 77
     }
   }
 
@@ -76,29 +77,73 @@ class GiStarSpec extends WordSpec with Matchers
     1,1,0,0,0,0,0,0,0,2,0,0,13,0,0,0,
     0,0,0,1,1,0,0,0,1,4,6,0,2,0,0,0,
     0,8,2,6,0,0,0,4,3,1,4,7,0,0,0,0)
-  val W = List(6, 6, 2, 12, 9, 2, 2, 1, 1)
 
   "GiStar" should
   {
-    "return z = 4,178523 for (8, 8)" ignore {
+    "return z = 4,178523 for (8, 8) (central position with 8 neighbors)" in {
+      val W = List(6, 6, 2, 12, 9, 2, 2, 1, 1)
       val n = X.length
       val avg = GiStar.calcAverageValue(X)
       val s = GiStar.calcStdDeviation(X)
+
       // multiplied by 1,000,000 because of floating point imprecision
-      GiStar.calcZ(W, n, avg, s) * 1000000 shouldBe 4178523f
+      round(GiStar.calcZ(W, n, avg, s) * 1000000) shouldBe 4178523.0
+    }
+
+    "return z = -0.23129 for (8, 16) (border position with 5 neighbors)" in {
+      val W = List(0, 0, 1, 0, 4, 3)
+      val n = X.length
+      val avg = GiStar.calcAverageValue(X)
+      val s = GiStar.calcStdDeviation(X)
+
+      round(GiStar.calcZ(W, n , avg, s) * 100000) shouldBe -23129.0
+    }
+
+    "return z = -0.26428 for (1, 1) (corner position with 3 neighbors)" in {
+      val W = List(1, 1, 0, 3)
+      val n = X.length
+      val avg = GiStar.calcAverageValue(X)
+      val s = GiStar.calcStdDeviation(X)
+
+      round(GiStar.calcZ(W, n , avg, s) * 100000) shouldBe -26428.0
+    }
+
+    "return z = 0.213201 for (2, 3) (another position with 8 neighbors)" in {
+      val W = List(0, 3, 0, 5, 0, 0, 1, 4, 0)
+      val n = X.length
+      val avg = GiStar.calcAverageValue(X)
+      val s = GiStar.calcStdDeviation(X)
+
+      round(GiStar.calcZ(W, n , avg, s) * 1000000) shouldBe 213201.0
     }
   }
 
 //   Test-Case from the original paper by Getis and Ord
-//    val X = List(-0.35f, -1.62f, -0.05f, 1.86f, 2.17f, 1.67f, 0.21f, -1.03f)
-//    val W = List(-0.35f, 1.86f, 2.17f, 1.67f, 0.21f)
+//    val X_o = List(-0.35f, -1.62f, -0.05f, 1.86f, 2.17f, 1.67f, 0.21f, -1.03f)
 //    "GiStar" should
 //      {
 //        "return z = 1.9629 for 2.17 as j" in {
-//          val n = X.length
-//          val avg = GiStar.calcAverageValueWithFloat(X)
-//          val s = GiStar.calcStdDeviationWithFloat(X)
-//          GiStar.calcZWithFloat(W, n, avg, s) shouldBe 1.9629
+//          val W_o = List(-0.35f, 1.86f, 2.17f, 1.67f, 0.21f)
+//          val n = X_o.length
+//          val avg = GiStar.calcAverageValueWithFloat(X_o)
+//          val s = GiStar.calcStdDeviationWithFloat(X_o)
+//          GiStar.calcZWithFloat(W_o, n, avg, s) shouldBe 1.9629f
+//        }
+//
+//        "return z = 1.8179 for 2.17 as j" in {
+//          val W_o = List(2.17f, 1.67f)
+//          val n = X_o.length
+//          val avg = GiStar.calcAverageValueWithFloat(X_o)
+//          val s = GiStar.calcStdDeviationWithFloat(X_o)
+//          GiStar.calcZWithFloat(W_o, n, avg, s) shouldBe 1.8179f
+//        }
+//
+//        "return z = 2.4078 for 2.17 as j" in {
+//          val W_o = List(2.17f, 1.67f, 1.86f)
+//          val n = X_o.length
+//          val avg = GiStar.calcAverageValueWithFloat(X_o)
+//          val s = GiStar.calcStdDeviationWithFloat(X_o)
+//          GiStar.calcZWithFloat(W_o, n, avg, s) shouldBe 2.4078f
 //        }
 //      }
 //  worked! (only with special float handling functions)
