16c0f3fd57c6d06e1070626e622e8ca6e1622dcd
elouin <sir@elou.in>
2017-04-25 14:12:32 +0200

    run.sh changed, so it fits the cluster config

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@
 # ./run.sh ./ 0.1 0.1
 
 # please insert correct path
-JAR=/data/home/s61569/mp.jar
+JAR=/data/home/<username>/mp.jar
 
 TIMESTAMP=$(date +%s)
 
@@ -21,9 +21,8 @@ OPTIONS="$OPTIONS --conf spark.task.maxFailures=20"
 OPTIONS="$OPTIONS --conf spark.shuffle.io.maxRetries=20"
 OPTIONS="$OPTIONS --conf spark.storage.memoryFraction=0.4"
 OPTIONS="$OPTIONS --conf spark.shuffle.memoryFraction=0.4"
-OPTIONS="$OPTIONS --deploy-mode client"
-OPTIONS="$OPTIONS --master spark://fb6-005-200.beuth-hochschule.de:7077"
-
+OPTIONS="$OPTIONS --deploy-mode cluster"
+OPTIONS="$OPTIONS --master yarn" 
 INPUT="yellow_tripdata_2015-01.csv,yellow_tripdata_2015-02.csv,yellow_tripdata_2015-03.csv,yellow_tripdata_2015-04.csv,yellow_tripdata_2015-05.csv,yellow_tripdata_2015-06.csv,yellow_tripdata_2015-07.csv,yellow_tripdata_2015-08.csv,yellow_tripdata_2015-09.csv,yellow_tripdata_2015-10.csv,yellow_tripdata_2015-11.csv,yellow_tripdata_2015-12.csv"
 
 spark-submit $OPTIONS --class Main $JAR $1 $2 0.001 7
